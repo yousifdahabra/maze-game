9b2d01e87b75ef3fe06f2d569c048086c0ff25ed
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-10-26 21:08:49 +0300

    Edit: Add event on start and end button and try to change div color when hits div

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 var btn = document.querySelector(".example");
+var start = document.getElementById("start");
+var end = document.getElementById("end");
 var start_game = false;
+var hit_div = false;
 const divs = document.querySelectorAll(".boundary");
 
 btn.innerHTML = 'Start'
@@ -13,11 +16,32 @@ divs.forEach((div) => {
     div.addEventListener("mouseover", (event) => {
         if(start_game){
             console.log('lose')
-
+            hit_div = true;
+            
         }else{
             console.log('must click start to play')
 
         }
 
     });
+    if(!start_game){
+        div.style.backgroundColor = "red";
+    }
   });
+start.addEventListener("mouseover", (event) => {
+    if(start_game){
+        hit_div = false
+        divs.style.backgroundColor = "#eeeeee";
+    }else{
+        console.log('must click start to play')
+    }
+});
+end.addEventListener("mouseover", (event) => {
+    if(start_game){
+        console.log('lose')
+        hit_div = true;
+    }else{
+        console.log('must click start to play')
+
+    }
+});
